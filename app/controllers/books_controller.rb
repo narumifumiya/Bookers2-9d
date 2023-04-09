7da57cdaaf7b0a772c941bdf968936460f9462eb
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-04-09 08:00:51 +0000

    並び替え機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,16 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all
+    if params[:latest]
+      @books = Book.latest
+    elsif params[:old]
+      @books = Book.old
+    elsif params[:star_count]
+      @books = Book.star_count
+    else
+      @books = Book.all
+    end
+    # @books = Book.all
     @book = Book.new
     @user = current_user
   end
--- app/models/book.rb
+++ app/models/book.rb
@@ -6,10 +6,14 @@ class Book < ApplicationRecord
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
+  scope :latest, -> {order(created_at: :desc)} #descは降順…作成日が新しい順になる(10,9,8...)
+  scope :old, -> {order(created_at: :asc)} #ascは昇順…作成日が古い順になる(1,2,3...)
+  scope :star_count, -> {order(star: :desc)} #starが多い順になる
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
   # 検索方法分岐
   def self.looks(search, word)
     if search == "perfect"
